c03899bd80e3832cf19cb3aca265e06108d6082c
gl7857 <gl7857@bs.amalnet.k12.il>
2024-10-22 19:00:21 +0300

    Ex022

--- src/Cart.java
+++ src/Cart.java
@@ -1,6 +1,5 @@
 public class Cart extends Light{
     private double chargingTime;
-
     public Cart(int carNumber,int age,int wheelsNumber,boolean shapeIsWheel,double emissionsPerMin,boolean hasEngine, double chargingTime){
         super(carNumber,age,wheelsNumber,shapeIsWheel,emissionsPerMin,hasEngine);
         this.chargingTime = chargingTime;
