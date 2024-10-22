2a551969493a31f69185720d034be89ce7c9b729
gl7857 <gl7857@bs.amalnet.k12.il>
2024-10-22 18:59:47 +0300

    Ex022

--- src/Light.java
+++ src/Light.java
@@ -1,5 +1,6 @@
 public class Light extends Vehicles {
     private boolean hasEngine;
+
     public Light(int carNumber,int age,int wheelsNumber,boolean shapeIsWheel,double emissionsPerMin,boolean hasEngine){
         super(carNumber,age,wheelsNumber,shapeIsWheel,emissionsPerMin);
         this.hasEngine = hasEngine;
